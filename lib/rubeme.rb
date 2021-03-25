cdccb1b2445fe506ea014150af0c26405cf18aff
mnbi <mnbi@users.noreply.github.com>
2021-03-25 20:18:25 +0900

    add files generated by bundler

---
+++ lib/rubeme.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubeme/version"
+
+module Rubeme
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/rubeme_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class RubemeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Rubeme::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
